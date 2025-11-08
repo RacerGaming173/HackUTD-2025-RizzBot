c1e7ebaa3c905baa27463edd0294acf4a1cba66e
RacerGaming173 <edison.b.cheah@gmail.com>
2025-11-08 15:52:34 -0600

    Add test Gemini prompt

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain what is 6 7 slang",
+)
+
+print(response.text)
