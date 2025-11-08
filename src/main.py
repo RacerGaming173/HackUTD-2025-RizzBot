f4713384b14809e08e9b271336c32a83cf141cb4
RacerGaming173 <edison.b.cheah@gmail.com>
2025-11-08 16:20:01 -0600

    Change to lightweight model

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from google import genai
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
 client = genai.Client()
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Explain what is 6 7 slang",
+    model="gemini-2.5-flash-lite",
+    contents="Explain how AI works in a few words",
 )
 
 print(response.text)
